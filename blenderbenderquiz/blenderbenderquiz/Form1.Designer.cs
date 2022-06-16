0f525cb34b231b0df231379b27404ae43db539cf
InfernalV <91879230+InfernalV@users.noreply.github.com>
2022-06-16 09:24:54 +0200

    2

--- blenderbenderquiz/blenderbenderquiz/Form1.Designer.cs
+++ blenderbenderquiz/blenderbenderquiz/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace blenderbenderquiz
 {
-    partial class Form1
+    partial class BlenderBenderQuizz
     {
         /// <summary>
         /// Required designer variable.
@@ -31,9 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.A = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.Points = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,53 +45,69 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.OrangeRed;
-            this.pictureBox1.Location = new System.Drawing.Point(817, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(758, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Padding = new System.Windows.Forms.Padding(3);
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.Size = new System.Drawing.Size(266, 246);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // A
             // 
-            this.button1.Location = new System.Drawing.Point(99, 333);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(278, 55);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.A.BackColor = System.Drawing.Color.OrangeRed;
+            this.A.ForeColor = System.Drawing.SystemColors.Info;
+            this.A.Location = new System.Drawing.Point(49, 341);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(76, 76);
+            this.A.TabIndex = 2;
+            this.A.Text = "A";
+            this.A.UseVisualStyleBackColor = false;
+            // 
+            // B
+            // 
+            this.B.BackColor = System.Drawing.Color.OrangeRed;
+            this.B.ForeColor = System.Drawing.SystemColors.Info;
+            this.B.Location = new System.Drawing.Point(49, 423);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(76, 76);
+            this.B.TabIndex = 3;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(99, 411);
+            this.button2.BackColor = System.Drawing.Color.OrangeRed;
+            this.button2.ForeColor = System.Drawing.SystemColors.Info;
+            this.button2.Location = new System.Drawing.Point(49, 505);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(278, 55);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Size = new System.Drawing.Size(76, 76);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "C";
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // Points
             // 
-            this.button3.Location = new System.Drawing.Point(99, 492);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(278, 55);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.Points.BackColor = System.Drawing.Color.OrangeRed;
+            this.Points.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Points.Location = new System.Drawing.Point(12, 12);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(50, 50);
+            this.Points.TabIndex = 5;
             // 
-            // Form1
+            // BlenderBenderQuizz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1034, 601);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(1036, 601);
+            this.Controls.Add(this.Points);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.A);
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "BlenderBenderQuizz";
+            this.Text = "BlenderBenderQuizz";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -101,9 +118,10 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button A;
+        private System.Windows.Forms.Button B;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar Points;
     }
 }
 
--- blenderbenderquiz/blenderbenderquiz/Form1.cs
+++ blenderbenderquiz/blenderbenderquiz/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace blenderbenderquiz
 {
-    public partial class Form1 : Form
+    public partial class BlenderBenderQuizz : Form
     {
-        public Form1()
+        public BlenderBenderQuizz()
         {
             InitializeComponent();
         }
